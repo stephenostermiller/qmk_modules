e884b34260638142b93bcc9ac750b21c83b3caf3
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2025-07-16 06:22:22 -0400

    Move mod test to get mod mask

--- ezovrd/ezovrd.h
+++ ezovrd/ezovrd.h
@@ -17,13 +17,13 @@
 #pragma once
 
 // Get the mods from the keycode and create a mask with BOTH the left and right modifiers
-#define EZOVRD_MODS_GET_MASK(kc) ((((kc) >> 8) & 0xF) | (((kc) >> 4) & 0xF0))
+#define EZOVRD_MODS_GET_MASK(kc) (IS_QK_MODS((uint16_t)(kc)) ? ((((kc) >> 8) & 0xF) | (((kc) >> 4) & 0xF0)) : 0)
 
 // Remove the bits from the mod_mask that match the modifiers on the keycode
-#define EZOVRD_GET_SUPPRESS(mod_mask, replacement_key) (IS_QK_MODS((uint16_t)(replacement_key))?(mod_mask - (mod_mask & EZOVRD_MODS_GET_MASK(replacement_key))):mod_mask)
+#define EZOVRD_GET_SUPPRESS(mod_mask, replacement_key) ((mod_mask) - ((mod_mask) & EZOVRD_MODS_GET_MASK(replacement_key)))
 
 #define ezovrd_make_basic(mod_mask, trigger_key, replacement_key) \
-    ((const key_override_t){.trigger_mods = mod_mask, .layers = ~0, .suppressed_mods = EZOVRD_GET_SUPPRESS(mod_mask, (replacement_key)), .options = ko_options_default, .negative_mod_mask = 0,\
+    ((const key_override_t){.trigger_mods = (mod_mask), .layers = ~0, .suppressed_mods = EZOVRD_GET_SUPPRESS((mod_mask), (replacement_key)), .options = ko_options_default, .negative_mod_mask = 0,\
     .custom_action  = ezovrd_key_event, .context = (void *)(replacement_key), .trigger = (trigger_key), .replacement = (replacement_key)})
 
 bool ezovrd_key_event(bool pressed, void *context);
